bae54cefa9cbeb3b0d4d7e66df63f7dce367ae20
mo-salah1998 <naija963@gmail.com>
2023-06-08 19:47:41 +0100

    react project Template clone :  https://www.figma.com/file/fOZfqUp1r6s1pHTvXztCUU/Figma-Website-Template---Landing-Page-(Free)-(Community)?type=design&node-id=108-1324&t=R85nxPwVrmHZNnKJ-0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import imag from './designer_1.png' ;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="navBare">
+
+            <div className="nav">
+                <ul>
+                    <li><a href="/home" className="nav-barlink" >Home</a></li>
+                    <li><a href="/about" className="nav-barlink">About</a></li>
+                    <li><a href="/contact" className="nav-barlink">Contact</a></li>
+                </ul>
+            </div>
+            <div className="logo">Landing</div>
+            <div> <button className="button-dark"> Buy Now </button></div>
+        </div>
+        <div className="container">
+            <img src={imag} className="imag" alt="aaa"/>
+            <div className="div1">
+                <h1 className="Title1">Introduce Your Product </h1>
+                <h1 className="Title1">Quickly & Effectively</h1>
+                <p className="p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus dolorum facere ipsum laudantium molestiae necessitatibus obcaecati porro! Asperiores autem culpa deleniti doloremque enim ex illum minima qui, rem voluptatum.
+                    <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit Aliquid amet asperiores dolor eaque earum</p>
+               <div className="button-group">
+                    <button className="button-dark">Purchase UI Kit</button>
+                    <button className="button-light">Learn More</button>
+               </div>
+            </div>
+        </div>
+
+
+
     </div>
   );
 }
